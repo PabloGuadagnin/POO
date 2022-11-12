f23aa5869680fefa4ff848e619db5fc8995a8c68
PabloGuadagnin <pabloguadagnin.aluno@unipampa.edu.br>
2022-11-11 22:06:26 -0300

    //

--- Projetos JAVA/POO/2022/11/14/Ex3.java
+++ Projetos JAVA/POO/2022/11/14/Ex3.java
@@ -4,24 +4,24 @@
  * 
  */
 
-public class Ex3 extends Exception {
+ public class Ex3 extends Exception {
 
-    public static void teste() throws MinhaExcecao {
-        throw new MinhaExcecao();
-    }
+  //  public static void teste() throws MinhaExcecao {
+  //      throw new MinhaExcecao();
+ //   }
 
-    public static void main(String[] args) {
-        MinhaExcecao me = null;
-        try {
-            System.out.println("try ");
-            teste();
-        } catch (MinhaExcecao e) {
-            System.out.println("catch ");
-            me = e;
-        } finally {
-            System.out.println("finally ");
-            throw me;
-        }
-        System.out.println("fim");
-    }
+ //   public static void main(String[] args) {
+ //       MinhaExcecao me = null;
+ //       try {
+    //        System.out.println("try ");
+   //         teste();
+    //    } catch (MinhaExcecao e) {
+     //       System.out.println("catch ");
+   //         me = e;
+    //    } finally {
+    //        System.out.println("finally ");
+    //        throw me;
+  //      }
+   //     System.out.println("fim");
+  //  }
 }
